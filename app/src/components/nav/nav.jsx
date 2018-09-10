006f7dbbecfc8d3e8c15ec5fcaa6b8c9b5e199b8
saywandeusher <ongsaywan@hotmail.com>
2018-09-10 15:27:14 +0800

    Navbar v1

--- app/src/components/nav/nav.jsx
+++ app/src/components/nav/nav.jsx
@@ -6,14 +6,25 @@ export default class Nav extends Component {
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-        </div>
+          <div className="nav-wrapper container nav_container">
+            <div className="row nav_row">
+              <div className="col s3">
+                <a href="home" className="brand-logo"><img src={require('../../images/Screen 1/FarmFresh Logo.png')} alt="FarmFresh_Logo" className="FarmFresh-img" /></a>
+              </div>
+              <div className="col s4">
+                  <form>
+                     <input type="text" placeholder="Search for a product or brand" className="searchbox" />
+                     <button class="btn blue darken-2 search-button" type="submit" name="action">
+                      <i class="material-icons">search</i>
+                     </button>
+                  </form>
+              </div>
+              <div className="col s2"></div>
+              <div className="col s3">
+                <a href="Shopping" className="cart-logo right"><img src={require('../../images/Screen 1/shopping-cart.png')} alt="Cart_Image" className="cart-img" /></a>
+              </div>
+            </div>
+          </div> 
       </nav>
     );
   }
