741e93764013d7066ff2b62cce1966fce57053ec
saywandeusher <ongsaywan@hotmail.com>
2018-09-10 23:34:12 +0800

    Scrolling effect

--- app/src/components/intro/intro.jsx
+++ app/src/components/intro/intro.jsx
@@ -5,10 +5,10 @@ export default class Intro extends Component {
 
   render() {
     return (
-      <div className="parallax-container center valign-wrapper">
-      <div className="parallax">
-        <img src={require('../../images/Screen 1/Banner Photo.jpg')} alt="Banner_photo" />
-      </div>
+      <div className="parallax-container center valign-wrapper zoom">
+        <div className="parallax">
+          <img src={require('../../images/Screen 1/Banner Photo.jpg')} alt="Banner_photo" />
+        </div>
     </div>
     );
   }
