216d323e4b24348c9bb6347cf9ccd44464bb213e
saywandeusher <ongsaywan@hotmail.com>
2018-09-10 18:12:47 +0800

    create promo page

--- app/src/components/promo-page/promo.jsx
+++ app/src/components/promo-page/promo.jsx
@@ -6,13 +6,13 @@ export default class Promo extends Component {
   render() {
     return ( 
       <section className="promo-section">
-      <div className="container">
-      <div className="row">
-          <div className="col s6">
-            <h3 className="promo-headings">FRESH FOOD FOR A FRESHER YOU</h3>
-            <h5>Quailty food, quailty life.</h5>
+      <div className="container promo-container">
+      <div className="row promo-row">
+          <div className="col s6 ">
+            <div className="promo-headings">FRESH FOOD FOR A FRESHER YOU</div>
+            <div className="promo-subheadings">Quailty food, quailty life.</div>
           </div>
-          <div className="col s6">
+          <div className="col s6 center">
           <img src={require('../../images/Screen 2/Side image.png')} alt="side_image" className="side-image" />
           </div>
         </div>
@@ -20,15 +20,24 @@ export default class Promo extends Component {
         <div className="row">
 
           <div className="col m4 s12">
-            <h5 className="center">Online anywhere</h5>
+            <div className="center">
+              <img src={require('../../images/Screen 2/clock.png')} alt="clock_image" className="promo-image" />
+            </div>
+            <div className="center promo-subheadings">Online anywhere</div>
           </div>
 
           <div className="col m4 s12">
-            <h5 className="center">Fast Delivery</h5>
+          <div className="center">
+              <img src={require('../../images/Screen 2/delivery-truck.png')} alt="delivery_truck_image" className="promo-image-truck" />
+            </div>
+            <div className="center promo-subheadings">Fast Delivery</div>
           </div>
 
           <div className="col m4 s12">
-            <h5 className="center">24/7 support</h5>
+          <div className="center">
+              <img src={require('../../images/Screen 2/support.png')} alt="support_image" className="promo-image" />
+            </div>
+            <div className="center promo-subheadings">24/7 support</div>
           </div>
         
       </div>
